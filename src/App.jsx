31be8089a65d6a2861fe9213cfdb222066e437c4
saiki12 <saikiranhs338@gmail.com>
2024-11-16 15:05:44 -0500

    feat(ui-redesign): implement input form, and ui redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Layout from './Components/Layout';
 import Nature from './Pages/HomePages';
-import Animal from './Pages/HomePages/Animal';
-import Recycling from './Pages/HomePages/Recycling';
-import Layout1 from './Components/Layout/Layout1';
-import Agriculture from './Pages/HomePages/Agriculture';
-import OceanLife from './Pages/HomePages/OceanLife';
-import Charity from './Pages/HomePages/Charity';
-import ClimateChange from './Pages/HomePages/ClimateChange';
 import AboutPage from './Pages/AboutPage/AboutPage';
 import Services from './Pages/ServicePages/Services';
 import Team from './Pages/TeamPages/Team';
@@ -18,7 +11,6 @@ import Contact from './Pages/Contact/Contact';
 import CustomCursor from './Components/CustomCursor/CustomCursor';
 import ScrollUpButton from './Components/ScrollUpButton';
 import ServiceDetails from './Pages/ServicePages/ServiceDetails';
-import Layout2 from './Components/Layout/Layout2';
 import 'aos/dist/aos.css';
 import Aos from 'aos';
 import Error404 from './Pages/ErrorPage';
@@ -44,10 +36,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           {/* home page  */}
           <Route index element={<Nature />} />
-          <Route path="/animal" element={<Animal />} />
-          <Route path="/recycling" element={<Recycling />} />
-          <Route path="/charity" element={<Charity />} />
-          <Route path="/climate-change" element={<ClimateChange />} />
           {/* other page  */}
           <Route path="/about" element={<AboutPage />} />
           <Route path="/services" element={<Services />} />
@@ -58,12 +46,6 @@ function App() {
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/error" element={<Error404 />} />
-        </Route>
-        <Route path="/" element={<Layout1 />}>
-          <Route path="/agriculture" element={<Agriculture />} />
-        </Route>
-        <Route path="/" element={<Layout2 />}>
-          <Route path="/ocean-life" element={<OceanLife />} />;
         </Route>
         <Route path="*" element={<Error404 />} />
       </Routes>
